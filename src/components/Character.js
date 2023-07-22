8aa214d019b92ad90ddc299d52e973c67275c5a8
sanjiro <defconx00@gmail.com>
2023-07-21 20:49:08 -0400

    show details button added

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,52 +1,72 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
 
-const Card = styled.div``
-const CharDiv = styled.div``
+const Card = styled.div`
+
+
+
+`;
+const CharDiv = styled.div`
+  
+`;
+
+// Styled component for red text
+const RedText = styled.span`
+  color: red;
+  display: flex;
+  flex-direction: column;
+`;
+
+// Styled component for character container with flex layout and borders
+
+const CharacterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 13px solid black;
+  padding: 10px;
+  width: 50%;
+  color: white;
+  border-radius: 13cqmin;
+  margin: 0 auto; /* Add this line to center the container horizontally */
+`;
+
 
 const Character = (props) => {
   const { char } = props;
+
+  // State to track whether details are shown or hidden
+  const [detailsVisible, setDetailsVisible] = useState(true);
+
+  // Function to toggle the visibility of details
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible);
+  };
+
   return (
     <Card>
-      {char.map((charmander) => {
-        return (
-            <CharDiv key={charmander}>
-            <h3>name: {charmander.name}</h3>
-            <p>Gender: {charmander.gender}</p>
-            <p>Birth Year: {charmander.birth_year}</p>
-          </CharDiv>
-        )
-      })}
+      <button onClick={toggleDetails}>
+        {detailsVisible ? "Hide Details" : "Show Details"}
+      </button>
+      {detailsVisible &&
+        char.map((charmander) => {
+          return (
+            <CharacterContainer key={charmander.name}>
+              {/* Apply the RedText styled component conditionally */}
+              <h3>
+                name:{" "}
+                {detailsVisible ? <RedText>{charmander.name}</RedText> : charmander.name}
+              </h3>
+              <p>Gender: {charmander.gender}</p>
+              <p>Birth Year: {charmander.birth_year}</p>
+              <p>Height: {charmander.height}</p>
+              <p>Mass: {charmander.mass}</p>
+              <p>Hair Color: {charmander.hair_color}</p>
+            </CharacterContainer>
+          );
+        })}
     </Card>
-  )
-}
-/*
-import React from "react";
-import styled from "styled-components";
+  );
+};
 
-const Card = styled.div;
-const CharDiv = styled.div;
-
-const Character = (props) => {
-  const { char } = props;
-  return (
-    <Card>
-      {char.map((charmander) => {
-        return (
-            <CharDiv key={charmander}>
-            <h3>name: {charmander.name}</h3>
-            <p>Gender: {charmander.gender}</p>
-            <p>Birth Year: {charmander.birth_year}</p>
-          </CharDiv>
-        )
-      })}
-    </Card>
-  )
-}
 export default Character;
 
-
-*/
-
-
-export default Character;
